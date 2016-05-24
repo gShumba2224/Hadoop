16b694ef51ed8b6d8c3c2be581a92d88dba84a5d
Jango Jowells <gsmshumba@gmail.com>
2016-05-24 06:29:26 +0100

    Completed Simple mapper, With Json Parser

--- src/main/java/TestJob/GMapper.java
+++ src/main/java/TestJob/GMapper.java
@@ -2,16 +2,21 @@ package TestJob; /**
  * Created by gman on 5/23/16.
  */
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import  org.apache.hadoop.mapreduce.Mapper.Context;
 
 import java.io.IOException;
 
-public class GMapper extends Mapper <Integer,Integer,IntWritable,IntWritable> {
+public class GMapper extends Mapper <LongWritable,MapWritable,IntWritable,IntWritable> {
 
     @Override
-    protected void map(Integer key,Integer value, Context context) throws IOException, InterruptedException{
-       context.write(new IntWritable(key),new IntWritable(value));
+    protected void map(LongWritable key,MapWritable value, Context context) throws IOException, InterruptedException{
+        Text propType = (Text)value.get(new Text("PropertyType"));
+        if (propType != null) {
+            int newKey = Integer.parseInt((propType).toString());
+            context.write(new IntWritable(newKey), new IntWritable(1));
+        }
     }
 }
--- src/main/java/TestJob/JobMofo.java
+++ src/main/java/TestJob/JobMofo.java
@@ -2,10 +2,14 @@ package TestJob;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
+import JsonParser.JsonMapper;
 /**
  * Created by gman on 5/23/16.
  */
@@ -13,13 +17,22 @@ public class JobMofo {
     public static void main (String[] args) throws  Exception {
         Job job = Job.getInstance(new Configuration(), "testJob");
         job.setJarByClass(JobMofo.class);
-        FileInputFormat.addInputPath(job,new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setMapperClass(GMapper.class);
+        Configuration mapConfig = new Configuration(false);
+        ChainMapper.addMapper(job,JsonMapper.class,LongWritable.class,Text.class,LongWritable.class,MapWritable.class,mapConfig);
+
+        mapConfig = new Configuration(false);
+        ChainMapper.addMapper(job,GMapper.class,LongWritable.class,MapWritable.class,IntWritable.class,IntWritable.class,mapConfig);
+
+        String in = "/home/gman/Documents/IntelijWorkspace/SparkStreaming/src/main/resources/Datasets/SourceData/CleanTrainingSet/";
+        String out = "/home/gman/Documents/IntelijWorkspace/SparkStreaming/src/main/resources/Datasets/SourceData/output";
+        FileInputFormat.addInputPath(job,new Path(in));
+        FileOutputFormat.setOutputPath(job, new Path(out));
+
         job.setCombinerClass(GReduce.class);
         job.setReducerClass(GReduce.class);
         job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(IntWritable.class);
+        job.waitForCompletion(true);
     }
 }
